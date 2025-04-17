61fa0d4b04d112932f29b6844eb5704bf26e62c5
bkay-stack <kevinbest1900@gmail.com>
2025-04-17 13:26:28 +0100

    Todo-items

--- src/components/todolist/TodoList.tsx
+++ src/components/todolist/TodoList.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 import "./todolist.style.css";
+import checkIcon from "../../assets/images/icon-check.svg";
+import deleteIcon from "../../assets/images/icon-cross.svg";
 const TodoList = ({ todos }: { todos: string[] }) => {
-  return <div className="todo-list"></div>;
+  return (
+    <div className="todo-list-item">
+      {todos.map((todo, index) => (
+        <div className="todo-list-items" key={index}>
+          <span className="span-btn">
+            <img src={checkIcon} alt="" />
+          </span>
+          <p>{todo}</p>
+          <div className="delete-icon">
+            <img src={deleteIcon} alt="" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;
