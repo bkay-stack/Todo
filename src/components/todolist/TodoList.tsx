4f27d13d576130a633a5631f42dc6ea33affb443
bkay-stack <kevinbest1900@gmail.com>
2025-04-23 16:25:29 +0100

    completed

--- src/components/todolist/TodoList.tsx
+++ src/components/todolist/TodoList.tsx
@@ -6,6 +6,7 @@ import deleteIcon from "../../assets/images/icon-cross.svg";
 type Todo = {
   id: number;
   text: string;
+  completed: boolean;
 };
 
 type TodoListProps = {
@@ -16,8 +17,20 @@ type TodoListProps = {
 const TodoList = ({ todos, setTodos }: TodoListProps) => {
   console.log(todos);
 
-  // Delete function
+  // completed Todos function
+  const completedTodos = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // variable to get the id of the todo
+    const id = Number(event.target.id);
+    const isChecked = event.target.checked;
 
+    // Update the completed status of the todo
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: isChecked } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  // Delete function
   const deleteTodo = (id: number) => {
     const updatedTodos = todos.filter((todo) => todo.id !== id);
     setTodos(updatedTodos);
@@ -36,8 +49,17 @@ const TodoList = ({ todos, setTodos }: TodoListProps) => {
           <div key={todo.id}>
             <div className="todo-list-items">
               <div className="todo-task">
-                <input type="checkbox" name="checker" className="checkers" />
-                <p>{todo.text}</p>
+                <input
+                  type="checkbox"
+                  name="checker"
+                  className="checkers"
+                  id={todo.id.toString()}
+                  checked={todo.completed}
+                  onChange={completedTodos}
+                />
+                <p className={todo.completed ? "strike-through" : ""}>
+                  {todo.text}
+                </p>
               </div>
               <div className="delete-icon">
                 <img
